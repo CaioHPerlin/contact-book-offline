2dca4f89f72dae59bfd89edb06ef26909a58d17d
willian <williancardoso00004@gmail.com>
2024-05-13 08:39:19 -0400

    feat: regiister evento

---
+++ views/registerEvento/main.js
@@ -0,0 +1,57 @@
+function adicionarOrgao() {
+    // Cria um novo elemento select
+    var select = document.createElement("select");
+    select.setAttribute("class", "form-select");
+    select.setAttribute("required", "");
+
+    // Adiciona opções ao select
+    var opcaoPadrao = new Option("Escolha o órgão", "", false, false);
+    var opcaoNenhum = new Option("Nenhum", "Nenhum", false, false);
+    select.options.add(opcaoPadrao);
+    select.options.add(opcaoNenhum);
+
+    // Cria um novo div para inserir o select
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-md-6 teste");
+    div.appendChild(select);
+
+    // Adiciona o div ao container de outros órgãos
+    document.getElementById("outrosOrgaos").appendChild(div);
+}
+
+
+
+// teste de interação
+const dadosContatos = [
+    { id: 1, nome: "João" },
+    { id: 2, nome: "Maria" },
+    { id: 3, nome: "Pedro" }
+];
+
+function adicionarContato() {
+    const nomeInput = document.getElementById("inputAddress").value;
+
+    if(nomeInput.trim() === "") {
+        alert("Por favor, insira um nome de contato válido.");
+        return;
+    }
+
+    const contatoEncontrado = dadosContatos.find(contato => contato.nome === nomeInput);
+    
+    let containerContatos = document.getElementById("containerContatos");
+  
+    if (!containerContatos) {
+        // Cria o contêiner se ele não existir
+        containerContatos = document.createElement("div");
+        containerContatos.id = "containerContatos";
+        document.body.appendChild(containerContatos);
+    }
+
+    if (contatoEncontrado) {
+
+        alert("contato adicionado com sucesso")
+       
+    } else {
+        alert("Contato não encontrado.");
+    }
+}
